ed116d64f31fa02357b104cbc3a0441ea82470e0
Rubylium <alexisoko0@gmail.com>
2022-01-03 20:42:06 +0100

    refactor(pain): Yes

--- client/module/modules/utils.lua
+++ client/module/modules/utils.lua
@@ -28,13 +28,13 @@ function Modules.Utils.Comma_value(amount)
     local formatted = amount
     local k
     while true do  
-      formatted, k = string.gsub(formatted, "^(-?%d+)(%d%d%d)", '%1,%2')
-      if (k==0) then
-        break
-      end
+        formatted, k = string.gsub(formatted, "^(-?%d+)(%d%d%d)", '%1,%2')
+        if (k==0) then
+            break
+        end
     end
     return formatted
-  end
+end
 
 local timer = GetGameTimer()
 Citizen.CreateThread(function()
