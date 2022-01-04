995b47ea2ab9e3d97c00d102df9e67e625b4fe05
Rubylium <alexisoko0@gmail.com>
2022-01-04 09:08:05 +0100

    feat(ui): Add custom font

--- client/module/modules/loader.lua
+++ client/module/modules/loader.lua
@@ -2,6 +2,9 @@ Modules.Loader = {}
 Modules.Loader.dictToLoadFirst = {
     {"ui_drift"},
 }
+Modules.Loader.FontToLoad = {
+    {"forza", "forza"},
+}
 
 function Modules.Loader.Run()
     while Modules.UI == nil do
@@ -9,5 +12,8 @@ function Modules.Loader.Run()
     end
     for k,v in pairs(Modules.Loader.dictToLoadFirst) do
         Modules.UI.LoadStreamDict(v[1])
+    end
+    for k,v in pairs(Modules.Loader.FontToLoad) do
+        Modules.UI.LoadFont(v)
     end
 end
